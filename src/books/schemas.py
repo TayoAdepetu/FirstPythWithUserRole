fbf1353e843b4782adaa80c963cf264820229ba2
Tayo <copyafrica93@gmail.com>
2025-01-31 16:35:40 +0100

    db restructuring

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -1,6 +1,35 @@
 from pydantic import BaseModel
+from datetime import datetime
+import uuid
 
-
+# model format to create
 class BookCreateModel(BaseModel):
     title: str
     author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+# model format to retrieve
+class Book(BaseModel):
+    id: int
+    uid: uuid
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+    created_at: datetime
+    updated_at: datetime
+
+# model format to update
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
